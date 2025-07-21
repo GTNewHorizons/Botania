e23ec4ce378d0ab5332a490b0b3bc4e7b124e506
boubou19 <miisterunknown@gmail.com>
2025-07-22 00:00:04 +0200

    restore methods for ItemCraftingHalo

--- src/main/java/vazkii/botania/common/item/ItemCraftingHalo.java
+++ src/main/java/vazkii/botania/common/item/ItemCraftingHalo.java
@@ -585,26 +585,35 @@ public class ItemCraftingHalo extends ItemMod implements ICraftAchievement {
 		return ModAchievements.craftingHaloCraft;
 	}
 
+	public void onItemCrafted(ItemCraftedEvent event) {
+		if(!(event.craftMatrix instanceof InventoryCraftingHalo))
+			return;
+
+		for(int i = 0; i < event.player.inventory.getSizeInventory(); i++) {
+			ItemStack stack = event.player.inventory.getStackInSlot(i);
+			if(stack != null && stack.getItem() instanceof ItemCraftingHalo)
+				saveRecipeToStack(event, stack);
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void onRenderWorldLast(RenderWorldLastEvent event) {
+		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+		ItemStack stack = player.getCurrentEquippedItem();
+		if(stack != null && stack.getItem() instanceof ItemCraftingHalo)
+			render(stack, player, event.partialTicks);
+	}
+
 	public class EventHandler{
 		@SubscribeEvent
-		public void onItemCrafted(ItemCraftedEvent event) {
-			if(!(event.craftMatrix instanceof InventoryCraftingHalo))
-				return;
-
-			for(int i = 0; i < event.player.inventory.getSizeInventory(); i++) {
-				ItemStack stack = event.player.inventory.getStackInSlot(i);
-				if(stack != null && stack.getItem() instanceof ItemCraftingHalo)
-					saveRecipeToStack(event, stack);
-			}
+		public void onItemCraftedWrapper(ItemCraftedEvent event) {
+			ItemCraftingHalo.this.onItemCrafted(event);
 		}
 
 		@SideOnly(Side.CLIENT)
 		@SubscribeEvent
-		public void onRenderWorldLast(RenderWorldLastEvent event) {
-			EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-			ItemStack stack = player.getCurrentEquippedItem();
-			if(stack != null && stack.getItem() instanceof ItemCraftingHalo)
-				render(stack, player, event.partialTicks);
+		public void onRenderWorldLastWrapper(RenderWorldLastEvent event) {
+			ItemCraftingHalo.this.onRenderWorldLast(event);
 		}
 	}
 
