95ef44f43c14e8ad38eb6f41ae5243836422855f
koolkrafter5 <creepersrgreen@gmail.com>
2026-03-20 18:57:15 -0400

    Clean up lexica info handler, make the transfer rect actually work by just rendering the lexica item rather than adding it as an itemstack, and show information when looking both for recipes and usages

--- src/main/java/vazkii/botania/client/integration/nei/recipe/RecipeHandlerLexicaBotania.java
+++ src/main/java/vazkii/botania/client/integration/nei/recipe/RecipeHandlerLexicaBotania.java
@@ -2,22 +2,22 @@
  * This class was created by <Vazkii>. It's distributed as
  * part of the Botania Mod. Get the Source Code in github:
  * https://github.com/Vazkii/Botania
- * 
+ * <p>
  * Botania is Open Source and distributed under the
  * Botania License: http://botaniamod.net/license.php
- * 
+ * <p>
  * File Created @ [12/12/2015, 23:25:47 (GMT)]
  */
 package vazkii.botania.client.integration.nei.recipe;
 
 import java.awt.Rectangle;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
@@ -34,116 +34,126 @@ import codechicken.nei.recipe.TemplateRecipeHandler;
 
 public class RecipeHandlerLexicaBotania extends TemplateRecipeHandler {
 
-	public class CachedLexicaBotaniaRecipe extends CachedRecipe {
+    private static final RenderItem renderItem = new RenderItem();
+    private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final ItemStack lexicaStack = new ItemStack(ModItems.lexicon);
 
-		public LexiconEntry entry;
-		public PositionedStack item;
-		public List<PositionedStack> otherStacks = new ArrayList<>();
+    public class CachedLexicaBotaniaRecipe extends CachedRecipe {
 
-		public CachedLexicaBotaniaRecipe(ItemStack stack, LexiconEntry entry) {
-			otherStacks.add(new PositionedStack(new ItemStack(ModItems.lexicon), 51, 5));
-			item = new PositionedStack(stack, 91, 5);
-			this.entry = entry;
-		}
+        public LexiconEntry entry;
+        public PositionedStack item;
 
-		@Override
-		public List<PositionedStack> getIngredients() {
-			return otherStacks;
-		}
+        public CachedLexicaBotaniaRecipe(ItemStack stack, LexiconEntry entry) {
+            item = new PositionedStack(stack, 91, 5);
+            this.entry = entry;
+        }
 
-		@Override
-		public PositionedStack getResult() {
-			return item;
-		}
+        @Override
+        public PositionedStack getResult() {
+            return item;
+        }
 
-		@Override
-		public List<PositionedStack> getOtherStacks() {
-			return otherStacks;
-		}
+        @Override
+        public boolean contains(Collection<PositionedStack> ingredients, ItemStack ingredient) {
+            return ingredient.getItem() == ModItems.lexicon;
+        }
 
-		@Override
-		public boolean contains(Collection<PositionedStack> ingredients, ItemStack ingredient) {
-			return ingredient.getItem() == ModItems.lexicon;
-		}
+    }
 
-	}
+    @Override
+    public String getRecipeName() {
+        return StatCollector.translateToLocal("botania.nei.lexica");
+    }
 
-	@Override
-	public String getRecipeName() {
-		return StatCollector.translateToLocal("botania.nei.lexica");
-	}
+    @Override
+    public String getOverlayIdentifier() {
+        return "botania.lexica";
+    }
 
-	@Override
-	public String getGuiTexture() {
-		return LibResources.GUI_NEI_BLANK;
-	}
-	
-	@Override
-	public void loadTransferRects() {
-		transferRects.add(new RecipeTransferRect(new Rectangle(50, 4, 18, 18), "botania.lexica"));
-	}
-	
-	@Override
-	public void drawBackground(int recipe) {
-		super.drawBackground(recipe);
-		
-		FontRenderer font = Minecraft.getMinecraft().fontRenderer;
-		CachedLexicaBotaniaRecipe recipeObj = ((CachedLexicaBotaniaRecipe) arecipes.get(recipe));
+    @Override
+    public String getGuiTexture() {
+        return LibResources.GUI_NEI_BLANK;
+    }
 
-		String s = EnumChatFormatting.UNDERLINE + StatCollector.translateToLocal(recipeObj.entry.getUnlocalizedName());
-		font.drawString(s, 82 - font.getStringWidth(s) / 2, 30, 4210752);
-		
-		KnowledgeType type = recipeObj.entry.getKnowledgeType();
-		s = type.color + StatCollector.translateToLocal(type.getUnlocalizedName()).replaceAll("\\&.", "");
-		font.drawString(s, 82 - font.getStringWidth(s) / 2, 42, 4210752);
-		
-		s = "\"" + StatCollector.translateToLocal(recipeObj.entry.getTagline()) + "\"";
-		PageText.renderText(5, 42, 160, 200, s);
-		
-		String key = LexiconRecipeMappings.stackToString(recipeObj.item.item);
-		String quickInfo = "botania.nei.quickInfo:" + key;
-		String quickInfoLocal = StatCollector.translateToLocal(quickInfo);
-		
-		if(GuiScreen.isShiftKeyDown() && GuiScreen.isCtrlKeyDown() && Minecraft.getMinecraft().gameSettings.advancedItemTooltips)
-			s = "name: " + key;
-		else if(quickInfo.equals(quickInfoLocal))
-			s = StatCollector.translateToLocal("botania.nei.lexicaNoInfo");
-		else {
-			s = StatCollector.translateToLocal("botania.nei.lexicaSeparator");
-			font.drawString(s, 82 - font.getStringWidth(s) / 2, 80, 4210752);
-			s = quickInfoLocal;
-		}
-		
-		PageText.renderText(5, 80, 160, 200, s);
-	}
+    @Override
+    public void loadTransferRects() {
+        transferRects.add(new RecipeTransferRect(new Rectangle(50, 4, 18, 18), getOverlayIdentifier()));
+    }
 
-	@Override
-	public void loadCraftingRecipes(String outputId, Object... results) {
-		if(outputId.equals("botania.lexica")) {
-			for(LexiconEntry entry : BotaniaAPI.getAllEntries()) {
-				List<ItemStack> stacks = entry.getDisplayedRecipes();
-				for(ItemStack stack : stacks)
-					arecipes.add(new CachedLexicaBotaniaRecipe(stack, entry));
-			}
-		} else super.loadCraftingRecipes(outputId, results);
-	}
+    @Override
+    public void drawBackground(int recipe) {
+        super.drawBackground(recipe);
 
-	@Override
-	public void loadCraftingRecipes(ItemStack result) {
-		for(LexiconEntry entry : BotaniaAPI.getAllEntries()) {
-			List<ItemStack> stacks = entry.getDisplayedRecipes();
-			for(ItemStack stack : stacks) {
-				String key1 = LexiconRecipeMappings.stackToString(stack);
-				String key2 = LexiconRecipeMappings.stackToString(result);
-				if(key1.equals(key2))
-					arecipes.add(new CachedLexicaBotaniaRecipe(stack, entry));
-			}
-		}
-	}
+        FontRenderer font = Minecraft.getMinecraft().fontRenderer;
+        CachedLexicaBotaniaRecipe recipeObj = ((CachedLexicaBotaniaRecipe) arecipes.get(recipe));
 
-	@Override
-	public void loadUsageRecipes(ItemStack ingredient) {
-		// NO-OP
-	}
+        renderItem.renderItemAndEffectIntoGUI(font, mc.getTextureManager(), lexicaStack, 51, 5);
+
+        String s = EnumChatFormatting.UNDERLINE + StatCollector.translateToLocal(recipeObj.entry.getUnlocalizedName());
+        font.drawString(s, 82 - font.getStringWidth(s) / 2, 30, 0x404040);
+
+        KnowledgeType type = recipeObj.entry.getKnowledgeType();
+        s = type.color + StatCollector.translateToLocal(type.getUnlocalizedName()).replaceAll("&.", "");
+        font.drawString(s, 82 - font.getStringWidth(s) / 2, 42, 0x404040);
+
+        s = "\"" + StatCollector.translateToLocal(recipeObj.entry.getTagline()) + "\"";
+        PageText.renderText(5, 42, 160, 200, s);
+
+        String key = LexiconRecipeMappings.stackToString(recipeObj.item.item);
+        String quickInfo = "botania.nei.quickInfo:" + key;
+        String quickInfoLocal = StatCollector.translateToLocal(quickInfo);
+
+        if (GuiScreen.isShiftKeyDown() && GuiScreen.isCtrlKeyDown() && Minecraft.getMinecraft().gameSettings.advancedItemTooltips)
+            s = "name: " + key;
+        else if (quickInfo.equals(quickInfoLocal))
+            s = StatCollector.translateToLocal("botania.nei.lexicaNoInfo");
+        else {
+            font.drawString(
+                    StatCollector.translateToLocal("botania.nei.lexicaSeparator"),
+                    82 - font.getStringWidth(s) / 2,
+                    80,
+                    0x404040);
+            s = quickInfoLocal;
+        }
+
+        PageText.renderText(5, 80, 160, 200, s);
+    }
+
+    @Override
+    public void loadCraftingRecipes(String outputId, Object... results) {
+        if (outputId.equals(getOverlayIdentifier())) {
+            for (LexiconEntry entry : BotaniaAPI.getAllEntries()) {
+                List<ItemStack> stacks = entry.getDisplayedRecipes();
+                for (ItemStack stack : stacks)
+                    arecipes.add(new CachedLexicaBotaniaRecipe(stack, entry));
+            }
+        } else {
+            super.loadCraftingRecipes(outputId, results);
+        }
+    }
+
+    @Override
+    public void loadCraftingRecipes(ItemStack result) {
+        for (LexiconEntry entry : BotaniaAPI.getAllEntries()) {
+            List<ItemStack> stacks = entry.getDisplayedRecipes();
+            for (ItemStack stack : stacks) {
+                String key1 = LexiconRecipeMappings.stackToString(stack);
+                String key2 = LexiconRecipeMappings.stackToString(result);
+                if (key1.equals(key2)) {
+                    arecipes.add(new CachedLexicaBotaniaRecipe(stack, entry));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void loadUsageRecipes(String outputId, Object... ingredients) {
+        loadCraftingRecipes(outputId, ingredients);
+    }
+
+    @Override
+    public void loadUsageRecipes(ItemStack ingredient) {
+        loadCraftingRecipes(ingredient);
+    }
 
 }
