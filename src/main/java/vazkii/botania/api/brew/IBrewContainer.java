c3cf349ef516ebfb80c7972b1a7cfb31299b4315
nicksitnikov <nicksitnikov@gmail.com>
2026-07-20 20:13:04 -0500

    Improve documentation for the brew API

--- src/main/java/vazkii/botania/api/brew/IBrewContainer.java
+++ src/main/java/vazkii/botania/api/brew/IBrewContainer.java
@@ -10,26 +10,32 @@
  */
 package vazkii.botania.api.brew;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 /**
- * An Item that implements this counts as a Brew Container, by which
- * it can be used a center item for brew recipes and can contain
- * a brew.
+ * An {@link Item} that implements this counts as a Brew Container, which allows it to be used a center item in the Botanical Brewery.
  */
 public interface IBrewContainer {
 
 	/**
-	 * Returs an ItemStack that should be an item that has the brew
-	 * passed in.
+	 * Fills an {@link ItemStack} of this container with a {@link Brew}.
+     * If the brew cannot be applied to this container, returns null instead.
+     *
+     * @param brew The brew which may or may not be applicable to this container.
+     * @param stack The stack of this item which is being filled. This stack is not mutated.
+     * @return A <i>new</i> {@link ItemStack} containing an {@link IBrewItem}, or null if this brew is not applicable.
+     * @apiNote The returned stack does not necessarily have to be the same {@link Item} as the empty container.
 	 */
-	public ItemStack getItemForBrew(Brew brew, ItemStack stack);
+    ItemStack getItemForBrew(Brew brew, ItemStack stack);
 
 	/**
-	 * Gets the cost to add this brew onto this container. Return -1
-	 * to not allow for the brew to be added. Normally you'd
-	 * use brew.getManaCost(stack);
+	 * Gets the cost to add this brew onto this container. The cost must be a positive number.
+     *
+     * @param brew The brew being applied, which must be applicable to this container. {@link IBrewContainer#getItemForBrew(Brew, ItemStack)} can be used to determine whether the brew is applicable.
+     * @param stack A stack of this empty container. This stack is not mutated.
+     * @return The mana cost for this recipe, which is a positive number.
 	 */
-	public int getManaCost(Brew brew, ItemStack stack);
+	int getManaCost(Brew brew, ItemStack stack);
 
 }
