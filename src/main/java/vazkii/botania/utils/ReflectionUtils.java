8386f3af0bf2641573a359ebfd40dd1c1b19d939
boubou19 <miisterunknown@gmail.com>
2026-08-02 16:12:33 +0200

    Cache reflection fields in SkyblockSkyRenderer

---
+++ src/main/java/vazkii/botania/utils/ReflectionUtils.java
@@ -0,0 +1,31 @@
+package vazkii.botania.utils;
+
+import java.lang.reflect.Field;
+
+public final class ReflectionUtils {
+
+	private ReflectionUtils() {}
+
+	public static Field findField(Class<?> clazz, String... names) {
+		for(String name : names) {
+			try {
+				Field field = clazz.getDeclaredField(name);
+				field.setAccessible(true);
+				return field;
+			} catch(NoSuchFieldException e) {
+				// Try the next candidate name
+			}
+		}
+		return null;
+	}
+
+	public static int getInt(Field field, Object instance) {
+		try {
+			return field.getInt(instance);
+		} catch(Exception e) {
+			return 0;
+		}
+	}
+
+
+}
